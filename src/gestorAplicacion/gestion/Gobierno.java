64e4640f45f0da3b5632f4be4463d59f2aabbb60
Miramirez22 <miramirez@unal.edu.co>
2022-05-30 15:39:09 -0500

    Create Gobierno.java

---
+++ src/gestorAplicacion/gestion/Gobierno.java
@@ -0,0 +1,14 @@
+package gestorAplicacion.gestion;
+
+public interface Gobierno{
+	/*
+	Descripcion: interface que se implementara en Desarrollador y Empresa
+	*/	
+
+
+
+
+	/* Este metodo calcula cuanto debe pagar una empresa al gobierno en impuestos*/
+	public abstract String calcularImpuesto();
+
+}
