eac0008009d913364fa5bea82f2f358824f9a943
Miramirez22 <miramirez@unal.edu.co>
2022-06-28 17:44:53 -0500

    Create Gobierno.py

---
+++ python/gestor_aplicacion/gestion/Gobierno.py
@@ -0,0 +1,12 @@
+﻿class Gobierno:
+    #	
+    #	Descripcion: interface que se implementara en Desarrollador y Empresa
+    #		
+
+
+
+
+    # Este metodo calcula cuanto debe pagar una empresa al gobierno en impuestos
+    def calcularImpuesto(self):
+        pass
+
